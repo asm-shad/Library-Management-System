40a355b07615acdb9fe8b805610b17b127a5b502
Asm Shad <asmshad@gmail.com>
2025-06-21 21:22:25 +0600

    Database connection established

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,22 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import config from "./config";
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(5000, ()=> {
+app.listen(config.port, ()=> {
     console.log(`😎 Server is running.`);
 })
 
 async function server() {
     try {
-        // await mongoose.connect()
+        await mongoose.connect(config.database_url!);
 
-        console.log("DB Connected on port ${5000");
+        console.log(`DB Connected on port ${config.port}`);
     } catch (error) {
         console.error(`Server Error ${server}`);
     }
